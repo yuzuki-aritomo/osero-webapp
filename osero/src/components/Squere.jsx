95fdb5c5fdd3073536d3c3a0987cad08e8cd11ce
yuzuki <yuzuki.aritomo@gmail.com>
2021-03-28 18:00:37 +0900

    boardのデザイン

--- osero/src/components/Squere.jsx
+++ osero/src/components/Squere.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+const style = {
+    'width': '50px',
+    'height': '50px',
+    'border': 'solid 1px #000',
+    'backgroundColor' : '#339933',
+    'outline': 'none',
+}
+
 class Squere extends React.Component {
     render(){
         return(
-            <button onClick={ ()=>this.props.onClick() }>
+            <button style={ style } onClick={ ()=>this.props.onClick() }>
                 { this.props.value }
             </button>
         )

b08681a61eb0fe084e3976c88508b1e346e0ecfd
yuzuki <yuzuki.aritomo@gmail.com>
2021-03-29 03:23:36 +0900

    オセロのコマの裏返しの実装

--- osero/src/components/Board.jsx
+++ osero/src/components/Board.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Squere from "./Squere";
 
+const NoPiece = 0;
+const BlackPiece = 1;
+const WhitePiece = 2;
+const CanPiece = 3;
+
 class Board extends React.Component {
     constructor(props){
         super(props);
-        //boardの初期化
-        const squares = Array(9).fill(0);
-        for(let i=0; i<9; i++){
-            squares[i] = Array(9).fill(0)
+        //boardの初期化と最初のコマの作成
+        const squares = Array(8).fill(NoPiece);
+        for(let i=0; i<8; i++){
+            squares[i] = Array(8).fill(NoPiece);
         }
+        squares[3][3] = WhitePiece;
+        squares[4][4] = WhitePiece;
+        squares[3][4] = BlackPiece;
+        squares[4][3] = BlackPiece;
         this.state = {
             squares: squares,
         };
@@ -16,14 +25,52 @@ class Board extends React.Component {
     //盤面の横列の作成
     renderSquereLine(i){
         const Line = [];
-        for(let j=0; j<9; j++){
+        for(let j=0; j<8; j++){
             Line.push(<Squere key={ i+j } value={ this.state.squares[i][j] } onClick={ ()=>this.handleClick(i, j) } />)
         };
         return Line
     }
-    //ボタンクリック処理
+    //ピースクリック処理
     handleClick(i, j){
+        if(!this.CheckPutPiece(i, j)){
+            return
+        }
+        this.PutPiece(i, j, BlackPiece)
+    }
+    //コマを置けるかのチェック
+    CheckPutPiece(i, j){
+        const squares = this.state.squares.slice();
+        if(squares[i][j] != NoPiece){
+            return false
+        }
+        return true
+    }
+    //ピースを置いたときの処理
+    PutPiece(i, j, PlayerPiece){
+        const EnemyPiece = PlayerPiece == BlackPiece ? WhitePiece : BlackPiece
+        const X = [ 0, 1, 1, 1, 0,-1,-1,-1];
+        const Y = [-1,-1, 0, 1, 1, 1, 0,-1];
         console.log(i, j);
+        const squares = this.state.squares.slice();
+        //8方向の相手のコマを裏返す
+        for(let k = 0; k<8; k++){
+            if(squares[i+X[k]][j+Y[k]] == EnemyPiece){
+                var x = i+X[k]
+                var y = j+Y[k]
+                var loop = 0
+                while(squares[x][y] == EnemyPiece){
+                    x += X[k]
+                    y += Y[k]
+                    loop += 1
+                    if(squares[x][y] == PlayerPiece){
+                        for(let l = 0; l<=loop; l++){
+                            squares[i + X[k]*l][j + Y[k]*l] = PlayerPiece
+                        }
+                    }
+                }
+            }
+        }
+        this.setState({squares: squares});
     }
     render(){
         return(
@@ -36,7 +83,6 @@ class Board extends React.Component {
                 <div>{ this.renderSquereLine(5) }</div>
                 <div>{ this.renderSquereLine(6) }</div>
                 <div>{ this.renderSquereLine(7) }</div>
-                <div>{ this.renderSquereLine(8) }</div>
             </div>
         );
     }
